bf7d542c385597b3d53b00746f6632be69649205
alfianguide@outlook.com <alfianguide@outlook.com>
2019-07-28 19:41:28 +0700

    first init

---
+++ app/src/main/java/com/alfianguide/kotlineverywheresemarang2019/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.alfianguide.kotlineverywheresemarang2019
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.db.classParser
+import org.jetbrains.anko.db.insert
+import org.jetbrains.anko.db.select
+import org.jetbrains.anko.sdk27.coroutines.onClick
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        main_btn_save.onClick {
+            saveData()
+        }
+        main_btn_refresh.onClick {
+            readData()
+        }
+
+
+    }
+
+    private fun readData() {
+        database.use {
+            val result = select(ManusiaContract.TABLE_HUMAN)
+            val list = result.parseList(classParser<ManusiaContract>())
+            Log.d("List", list.toString())
+        }
+    }
+
+    private fun saveData() {
+        database.use {
+            insert(
+                ManusiaContract.TABLE_HUMAN,
+                ManusiaContract.NAME
+                        to main_edt_name.text.toString(),
+                ManusiaContract.ADDRESS
+                        to main_edt_address.text.toString()
+                )
+        }
+    }
+}
---
+++ app/src/main/java/com/alfianguide/kotlineverywheresemarang2019/ManusiaContract.kt
@@ -0,0 +1,14 @@
+package com.alfianguide.kotlineverywheresemarang2019
+
+data class ManusiaContract(
+    val id:Long?,
+    val nama:String,
+    val address:String
+){
+    companion object{
+        const val TABLE_HUMAN = "table_human"
+        const val ID = "id"
+        const val NAME = "name"
+        const val ADDRESS = "address"
+    }
+}
